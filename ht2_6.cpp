9d95b3b81adf6b625c546ebd2b679544da0bbcb4
baranov-i02 <58187199+baranov-i02@users.noreply.github.com>
2019-12-09 11:52:44 +0300

    Add files via upload

---
+++ ht2_6.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x,y,z,t,max,min,ra,r,max1,min1,max2,min2,ra1,r1;
+	cout << "Hello, dear friend!\n" << "Please, enter your numbers: ";
+	cin >> x >> y >> z >> t;
+	if (x >= y){
+		max1 = x;
+		min1 = y;
+	}
+		else{
+		max1 = y;
+		min1 = x;
+	}
+	if (z>=t){
+		max2 = z; 
+		min2 = t;
+	}
+		else{
+		max2 = t;
+		min2 = z;
+	}
+	if (max1 >= max2){
+		max = max1;
+		ra = max2;
+	}
+		else{
+		max = max2;
+		ra = max1;
+	}
+	if (min1 >= min2){
+		min = min2;
+		r = min1;
+	}
+		else{
+		min = min1;
+		r = min2;
+	}
+	if (ra >=r){
+		ra1 = ra;
+		r1 = r;
+	}
+		else{
+		ra1 = r;
+		r1 = ra;
+	}
+	cout << max << ra1 << r1 << min;
+	return 0;
+}
+
